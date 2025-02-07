6f5ba6877dcfc275c0e291932a3cdd0587f405ef
abdulbasit <proabdulbasit.me@gmail.com>
2025-02-07 21:51:20 +0500

    fIcon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,11 @@ import { ToastProvider } from "../components/ui/toast"
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/ficon.png" sizes="any" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <title>Sigmatic Trading</title>
+      </head>
       <body>
         {children}
         <ToastProvider />
